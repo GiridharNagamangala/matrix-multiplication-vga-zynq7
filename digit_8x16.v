7ae8e7c3c0e7ba3876a72c8ebb51f74e9f4d9e22
GiridharNagamangala <nagamangala.giridhar@gmail.com>
2025-11-21 16:19:14 +0530

    Non-synthesizable commit
    Created module digit_8x16 to map BCD to pixel map, yet to fix clock enable and datapath issues

---
+++ digit_8x16.v
@@ -0,0 +1,33 @@
+module digit_8x16 #(
+    parameter XSTART = 100,
+    parameter YSTART = 50
+)(
+	input wire clk,
+    input wire clk_en,
+    input wire [10:0] h_count,
+    input wire [10:0] v_count,
+	input wire [3:0]  bcd,
+	output wire [3:0] vga_r,
+    output wire [3:0] vga_g,
+    output wire [3:0] vga_b
+);
+
+    wire [3:0] row = v_count - YSTART;
+    wire [2:0] col = h_count - XSTART;
+    wire factive = (h_count >= XSTART) && (h_count < XSTART + 8) && (v_count >= YSTART) && (v_count < YSTART + 16);
+    wire fpixout;
+
+    // Instance
+    vga_digit_rom U1 (
+        .clk(clk),
+        .digit_code(bcd),
+        .row(row),
+        .col(col),
+        .pixel(fpixout)
+    );
+
+    assign vga_r = factive && fpixout ? 4'hf : 4'h0;
+    assign vga_g = factive && fpixout ? 4'hf : 4'h0;
+    assign vga_b = factive && fpixout ? 4'hf : 4'h0;
+
+endmodule
--- vga_rtl_top.v
+++ vga_rtl_top.v
@@ -43,8 +43,23 @@ module vga_rtl_top (
         .v_count(v_count)
     );
 
-    assign vga_r = (((h_count < 305 && h_count > 300) || (h_count < 600 && h_count > 595)) && (v_count < 500 && v_count > 200)) ? 4'hf : 4'h0; // (143, 784), (35, 515)
-    assign vga_g = (((h_count < 305 && h_count > 300) || (h_count < 600 && h_count > 595)) && (v_count < 500 && v_count > 200)) ? 4'hf : 4'h0; // (143, 784), (35, 515)
-    assign vga_b = (((h_count < 305 && h_count > 300) || (h_count < 600 && h_count > 595)) && (v_count < 500 && v_count > 200)) ? 4'hf : 4'h0; // (143, 784), (35, 515)
+    // assign vga_r = (((h_count < 305 && h_count > 300) || (h_count < 600 && h_count > 595)) && (v_count < 500 && v_count > 200)) ? 4'hf : 4'h0; // (143, 784), (35, 515)
+    // assign vga_g = (((h_count < 305 && h_count > 300) || (h_count < 600 && h_count > 595)) && (v_count < 500 && v_count > 200)) ? 4'hf : 4'h0; // (143, 784), (35, 515)
+    // assign vga_b = (((h_count < 305 && h_count > 300) || (h_count < 600 && h_count > 595)) && (v_count < 500 && v_count > 200)) ? 4'hf : 4'h0; // (143, 784), (35, 515)
+
+    // Digit display instance (added bcd code for digit '5' temporarily)
+    digit_8x16 #(
+        .XSTART(100),
+        .YSTART(50)
+    ) digit_display_inst (
+        .clk(clk),
+        .clk_en(pixel_clk),
+        .h_count(h_count),
+        .v_count(v_count),
+        .bcd(4'h5), // BCD code for digit '5'
+        .vga_r(vga_r),
+        .vga_g(vga_g),
+        .vga_b(vga_b)
+    );
 
 endmodule
