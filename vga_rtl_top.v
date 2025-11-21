401c1d6258f1d1be1bc14b1fdd1dd8ff08416eef
GiridharNagamangala <nagamangala.giridhar@gmail.com>
2025-11-21 18:30:47 +0530

    Displayed sample matrix
    should try and replace these instantiations with a generate or for block
    should change coordinates
    number 2 is mirrored

--- vga_rtl_top.v
+++ vga_rtl_top.v
@@ -1,7 +1,7 @@
 module vga_rtl_top #(
     parameter X11 = 100, parameter X21 = 200, parameter X31 = 300,
-    parameter X12 = 108, parameter X22 = 208, parameter X23 = 308,
-    parameter X13 = 116, parameter X32 = 216, parameter X33 = 316,
+    parameter X12 = 108, parameter X22 = 208, parameter X32 = 308,
+    parameter X13 = 116, parameter X23 = 216, parameter X33 = 316,
     parameter Y1 = 50, parameter Y2 = 70, parameter Y3 = 90
 )(
     input  wire clk,         // System clock input
@@ -18,7 +18,9 @@ module vga_rtl_top #(
     // Clock divider instance to generate pixel clock from system clock
     wire pixel_clk, bram_clk, v_en;
     wire [10:0] h_count, v_count;
-    wire p11, p12, p13, p21, p22, p23, p31, p32, p33;
+    wire p111, p112, p113, p121, p122, p123, p131, p132, p133,
+         p211, p212, p213, p221, p222, p223, p231, p232, p233,
+         p311, p312, p313, p321, p322, p323, p331, p332, p333;
 
     clk_divider #(
         .DIVIDE_BY(4) // Adjust this value based on system clock frequency to get desired pixel clock
@@ -51,108 +53,332 @@ module vga_rtl_top #(
         .v_count(v_count)
     );
 
-    // Digit display instance (added bcd code for number '91' temporarily)
+    // Number 1 digit 1
     digit_8x16 #(
         .XSTART(X11),
         .YSTART(Y1)
-    ) digit_display_inst11 (
+    ) digit_display_inst111 (
+        .clk(bram_clk),
+        .h_count(h_count),
+        .v_count(v_count),
+        .bcd(4'h1), // BCD code for digit '9'
+        .pixon(p111)
+    );
+
+    // number 1 digit 2
+    digit_8x16 #(
+        .XSTART(X12),
+        .YSTART(Y1)
+    ) digit_display_inst112 (
+        .clk(bram_clk),
+        .h_count(h_count),
+        .v_count(v_count),
+        .bcd(4'h2), // BCD code for digit '9'
+        .pixon(p112)
+    );
+
+    // number 1 digit 3
+    digit_8x16 #(
+        .XSTART(X13),
+        .YSTART(Y1)
+    ) digit_display_inst113 (
+        .clk(bram_clk),
+        .h_count(h_count),
+        .v_count(v_count),
+        .bcd(4'h3), // BCD code for digit '9'
+        .pixon(p113)
+    );
+
+    // number 2 digit 1
+    digit_8x16 #(
+        .XSTART(X21),
+        .YSTART(Y1)
+    ) digit_display_inst121 (
+        .clk(bram_clk),
+        .h_count(h_count),
+        .v_count(v_count),
+        .bcd(4'h4), // BCD code for digit '9'
+        .pixon(p121)
+    );
+
+    // number 2 digit 2
+    digit_8x16 #(
+        .XSTART(X22),
+        .YSTART(Y1)
+    ) digit_display_inst122 (
+        .clk(bram_clk),
+        .h_count(h_count),
+        .v_count(v_count),
+        .bcd(4'h5), // BCD code for digit '9'
+        .pixon(p122)
+    );
+
+    // number 2 digit 3
+    digit_8x16 #(
+        .XSTART(X23),
+        .YSTART(Y1)
+    ) digit_display_inst123 (
+        .clk(bram_clk),
+        .h_count(h_count),
+        .v_count(v_count),
+        .bcd(4'h6), // BCD code for digit '9'
+        .pixon(p123)
+    );
+
+    // number 3 digit 1
+    digit_8x16 #(
+        .XSTART(X31),
+        .YSTART(Y1)
+    ) digit_display_inst131 (
+        .clk(bram_clk),
+        .h_count(h_count),
+        .v_count(v_count),
+        .bcd(4'h7), // BCD code for digit '9'
+        .pixon(p131)
+    );
+
+    // number 3 digit 2
+    digit_8x16 #(
+        .XSTART(X32),
+        .YSTART(Y1)
+    ) digit_display_inst132 (
+        .clk(bram_clk),
+        .h_count(h_count),
+        .v_count(v_count),
+        .bcd(4'h8), // BCD code for digit '9'
+        .pixon(p132)
+    );
+
+    // number 3 digit 3
+    digit_8x16 #(
+        .XSTART(X33),
+        .YSTART(Y1)
+    ) digit_display_inst133 (
         .clk(bram_clk),
         .h_count(h_count),
         .v_count(v_count),
         .bcd(4'h9), // BCD code for digit '9'
-        .pixon(p11)
+        .pixon(p133)
     );
 
+    // Number 4 digit 1
+    digit_8x16 #(
+        .XSTART(X11),
+        .YSTART(Y2)
+    ) digit_display_inst211 (
+        .clk(bram_clk),
+        .h_count(h_count),
+        .v_count(v_count),
+        .bcd(4'h1), // BCD code for digit '9'
+        .pixon(p211)
+    );
+
+    // number 4 digit 2
     digit_8x16 #(
         .XSTART(X12),
-        .YSTART(Y1)
-    ) digit_display_inst12 (
+        .YSTART(Y2)
+    ) digit_display_inst212 (
         .clk(bram_clk),
         .h_count(h_count),
         .v_count(v_count),
-        .bcd(4'h1), // BCD code for digit '9'
-        .pixon(p12)
+        .bcd(4'h2), // BCD code for digit '9'
+        .pixon(p212)
     );
 
+    // number 4 digit 3
     digit_8x16 #(
         .XSTART(X13),
-        .YSTART(Y1)
-    ) digit_display_inst13 (
+        .YSTART(Y2)
+    ) digit_display_inst213 (
         .clk(bram_clk),
         .h_count(h_count),
         .v_count(v_count),
-        .bcd(4'h1), // BCD code for digit '9'
-        .pixon(p13)
+        .bcd(4'h3), // BCD code for digit '9'
+        .pixon(p213)
     );
 
+    // number 5 digit 1
     digit_8x16 #(
         .XSTART(X21),
         .YSTART(Y2)
-    ) digit_display_inst21 (
+    ) digit_display_inst221 (
         .clk(bram_clk),
         .h_count(h_count),
         .v_count(v_count),
-        .bcd(4'h1), // BCD code for digit '9'
-        .pixon(p21)
+        .bcd(4'h4), // BCD code for digit '9'
+        .pixon(p221)
     );
 
+    // number 5 digit 2
     digit_8x16 #(
         .XSTART(X22),
         .YSTART(Y2)
-    ) digit_display_inst22 (
+    ) digit_display_inst222 (
+        .clk(bram_clk),
+        .h_count(h_count),
+        .v_count(v_count),
+        .bcd(4'h5), // BCD code for digit '9'
+        .pixon(p222)
+    );
+
+    // number 5 digit 3
+    digit_8x16 #(
+        .XSTART(X23),
+        .YSTART(Y2)
+    ) digit_display_inst223 (
+        .clk(bram_clk),
+        .h_count(h_count),
+        .v_count(v_count),
+        .bcd(4'h6), // BCD code for digit '9'
+        .pixon(p223)
+    );
+
+    // number 6 digit 1
+    digit_8x16 #(
+        .XSTART(X31),
+        .YSTART(Y2)
+    ) digit_display_inst231 (
+        .clk(bram_clk),
+        .h_count(h_count),
+        .v_count(v_count),
+        .bcd(4'h7), // BCD code for digit '9'
+        .pixon(p231)
+    );
+
+    // number 6 digit 2
+    digit_8x16 #(
+        .XSTART(X32),
+        .YSTART(Y2)
+    ) digit_display_inst232 (
+        .clk(bram_clk),
+        .h_count(h_count),
+        .v_count(v_count),
+        .bcd(4'h8), // BCD code for digit '9'
+        .pixon(p232)
+    );
+
+    // number 6 digit 3
+    digit_8x16 #(
+        .XSTART(X33),
+        .YSTART(Y2)
+    ) digit_display_inst233 (
+        .clk(bram_clk),
+        .h_count(h_count),
+        .v_count(v_count),
+        .bcd(4'h9), // BCD code for digit '9'
+        .pixon(p233)
+    );
+
+    // Number 7 digit 1
+    digit_8x16 #(
+        .XSTART(X11),
+        .YSTART(Y3)
+    ) digit_display_inst311 (
         .clk(bram_clk),
         .h_count(h_count),
         .v_count(v_count),
         .bcd(4'h1), // BCD code for digit '9'
-        .pixon(p22)
+        .pixon(p311)
     );
 
+    // number 7 digit 2
+    digit_8x16 #(
+        .XSTART(X12),
+        .YSTART(Y3)
+    ) digit_display_inst312 (
+        .clk(bram_clk),
+        .h_count(h_count),
+        .v_count(v_count),
+        .bcd(4'h2), // BCD code for digit '9'
+        .pixon(p312)
+    );
+
+    // number 7 digit 3
+    digit_8x16 #(
+        .XSTART(X13),
+        .YSTART(Y3)
+    ) digit_display_inst313 (
+        .clk(bram_clk),
+        .h_count(h_count),
+        .v_count(v_count),
+        .bcd(4'h3), // BCD code for digit '9'
+        .pixon(p313)
+    );
+
+    // number 8 digit 1
+    digit_8x16 #(
+        .XSTART(X21),
+        .YSTART(Y3)
+    ) digit_display_inst321 (
+        .clk(bram_clk),
+        .h_count(h_count),
+        .v_count(v_count),
+        .bcd(4'h4), // BCD code for digit '9'
+        .pixon(p321)
+    );
+
+    // number 8 digit 2
+    digit_8x16 #(
+        .XSTART(X22),
+        .YSTART(Y3)
+    ) digit_display_inst322 (
+        .clk(bram_clk),
+        .h_count(h_count),
+        .v_count(v_count),
+        .bcd(4'h5), // BCD code for digit '9'
+        .pixon(p322)
+    );
+
+    // number 8 digit 3
     digit_8x16 #(
         .XSTART(X23),
         .YSTART(Y3)
-    ) digit_display_inst23 (
+    ) digit_display_inst323 (
         .clk(bram_clk),
         .h_count(h_count),
         .v_count(v_count),
-        .bcd(4'h1), // BCD code for digit '9'
-        .pixon(p23)
+        .bcd(4'h6), // BCD code for digit '9'
+        .pixon(p323)
     );
 
+    // number 9 digit 1
     digit_8x16 #(
         .XSTART(X31),
         .YSTART(Y3)
-    ) digit_display_inst31 (
+    ) digit_display_inst331 (
         .clk(bram_clk),
         .h_count(h_count),
         .v_count(v_count),
-        .bcd(4'h1), // BCD code for digit '9'
-        .pixon(p31)
+        .bcd(4'h7), // BCD code for digit '9'
+        .pixon(p331)
     );
 
+    // number 9 digit 2
     digit_8x16 #(
         .XSTART(X32),
         .YSTART(Y3)
-    ) digit_display_inst32 (
+    ) digit_display_inst332 (
         .clk(bram_clk),
         .h_count(h_count),
         .v_count(v_count),
-        .bcd(4'h1), // BCD code for digit '9'
-        .pixon(p32)
+        .bcd(4'h8), // BCD code for digit '9'
+        .pixon(p332)
     );
 
+    // number 9 digit 3
     digit_8x16 #(
         .XSTART(X33),
         .YSTART(Y3)
-    ) digit_display_inst33 (
+    ) digit_display_inst333 (
         .clk(bram_clk),
         .h_count(h_count),
         .v_count(v_count),
-        .bcd(4'h1), // BCD code for digit '9'
-        .pixon(p33)
+        .bcd(4'h9), // BCD code for digit '9'
+        .pixon(p333)
     );
 
-    assign vga_r = (p11 || p12 || p13 || p21 || p22 || p23 || p31 || p32 || p33) ? 4'hf : 4'h0;
-    assign vga_g = (p11 || p12 || p13 || p21 || p22 || p23 || p31 || p32 || p33) ? 4'hf : 4'h0;
-    assign vga_b = (p11 || p12 || p13 || p21 || p22 || p23 || p31 || p32 || p33) ? 4'hf : 4'h0;
+    assign vga_r = (p111 || p112 || p113 || p121 || p122 || p123 || p131 || p132 || p133 || p211 || p212 || p213 || p221 || p222 || p223 || p231 || p232 || p233 || p311 || p312 || p313 || p321 || p322 || p323 || p331 || p332 || p333) ? 4'hf : 4'h0;
+    assign vga_g = (p111 || p112 || p113 || p121 || p122 || p123 || p131 || p132 || p133 || p211 || p212 || p213 || p221 || p222 || p223 || p231 || p232 || p233 || p311 || p312 || p313 || p321 || p322 || p323 || p331 || p332 || p333) ? 4'hf : 4'h0;
+    assign vga_b = (p111 || p112 || p113 || p121 || p122 || p123 || p131 || p132 || p133 || p211 || p212 || p213 || p221 || p222 || p223 || p231 || p232 || p233 || p311 || p312 || p313 || p321 || p322 || p323 || p331 || p332 || p333) ? 4'hf : 4'h0;
 
 endmodule
