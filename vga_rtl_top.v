d2f798f1ee332a4e994e251c5b18911e0528037d
GiridharNagamangala <nagamangala.giridhar@gmail.com>
2025-11-22 16:07:56 +0530

    Updated top.v

--- vga_rtl_top.v
+++ vga_rtl_top.v
@@ -1,13 +1,13 @@
 module vga_rtl_top #(
-    // parameter X11 = 100, parameter X21 = 200, parameter X31 = 300,
-    // parameter X12 = 108, parameter X22 = 208, parameter X32 = 308,
-    // parameter X13 = 116, parameter X23 = 216, parameter X33 = 316,
-    // parameter Y1 = 50, parameter Y2 = 70, parameter Y3 = 90
     parameter X0 = 100, parameter Y0 = 50,
-    parameter MATRIX_N = 3, parameter MATRIX_M = 3, parameter DIGITS = 5
+    parameter MATRIX_N = 3, parameter MATRIX_M = 3, parameter DIGITS = 5, WIDTH = 16
 )(
     input  wire clk,         // System clock input
     input  wire reset,     // Active low reset
+    input read_ready,
+    input [MATRIX_M*MATRIX_N*WIDTH - 1:0] matrix_a,
+    input [MATRIX_M*MATRIX_N*WIDTH - 1:0] matrix_b,
+    output wire compute_done,
     output wire hsync,       // Horizontal sync output
     output wire vsync,       // Vertical sync output
     output wire hblank,      // Horizontal blanking signal
@@ -22,6 +22,9 @@ module vga_rtl_top #(
     wire [10:0] h_count, v_count;
     wire [(MATRIX_M * MATRIX_N * DIGITS * 3)-1:0] pixel;
 
+    // Register to store Matrix output numbers
+    wire [WIDTH-1:0] matrices [0:MATRIX_N-1][0:(MATRIX_M*3)-1];
+
     clk_divider #(
         .DIVIDE_BY(4) // Adjust this value based on system clock frequency to get desired pixel clock
     ) clk_div_inst (
@@ -29,6 +32,48 @@ module vga_rtl_top #(
         .reset(reset),
         .clk_out(pixel_clk),
         .clk_ram(bram_clk)
+    );
+
+    // Cannon's algorithm Matrix Multpiplication module
+    cannon #(
+        .N(3),
+        .WIDTH(16)
+    ) cannon_inst (
+        .clk(clk),
+        .reset(reset),
+        .start(1'b1),
+        .read_ready(read_ready),
+        .mat_a_in(matrix_a),
+        .mat_b_in(matrix_b),
+        .output_valid(compute_done),
+        .mata1(matrices[0][0]),
+        .mata2(matrices[0][1]),
+        .mata3(matrices[0][2]),
+        .mata4(matrices[1][0]),
+        .mata5(matrices[1][1]),
+        .mata6(matrices[1][2]),
+        .mata7(matrices[2][0]),
+        .mata8(matrices[2][1]),
+        .mata9(matrices[2][2]),
+        .matb1(matrices[0][3]),
+        .matb2(matrices[0][4]),
+        .matb3(matrices[0][5]),
+        .matb4(matrices[1][3]),
+        .matb5(matrices[1][4]),
+        .matb6(matrices[1][5]),
+        .matb7(matrices[2][3]),
+        .matb8(matrices[2][4]),
+        .matb9(matrices[2][5]),
+        .stage0(matrices[0][6]),
+        .stage1(matrices[0][7]),
+        .stage2(matrices[0][8]),
+        .stage3(matrices[1][6]),
+        .stage4(matrices[1][7]),
+        .stage5(matrices[1][8]),
+        .stage6(matrices[2][6]),
+        .stage7(matrices[2][7]),
+        .stage8(matrices[2][8]),
+        .serial_c_out() // Unused as of now
     );
 
     // Horizontal counter instance
@@ -58,6 +103,13 @@ module vga_rtl_top #(
         for (i = 0; i < MATRIX_N; i = i + 1) begin : row_loop
             for (j = 0; j < 3; j = j + 1) begin : matrix_loop
                 for (k = 0; k < MATRIX_M; k = k + 1) begin : col_loop
+                    wire [(DIGITS*4)-1:0] bcd_num;
+                    bin2bcd #(
+                        .W(WIDTH)
+                    ) bcd_inst (
+                        .bin(matrices[i][j * MATRIX_M + k]),
+                        .bcd(bcd_num)
+                    );
                     for (l = 0; l < DIGITS; l = l + 1) begin : digit_loop
                         digit_8x16 #(
                             .XSTART(X0 + (l * 8) + (k * 55) + (j * 165)),
@@ -66,7 +118,7 @@ module vga_rtl_top #(
                             .clk(bram_clk),
                             .h_count(h_count),
                             .v_count(v_count),
-                            .bcd(4'h5),
+                            .bcd(bcd_num[(l*4) +: 4]),
                             .pixon(pixel[(i * MATRIX_M * DIGITS * 3) + (j * MATRIX_M * DIGITS) + (k * DIGITS) + l])
                         );
                     end
