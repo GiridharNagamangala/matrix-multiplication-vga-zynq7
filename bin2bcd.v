eead2ba89fda8baac45fc1e48e433e0df5cd6db1
GiridharNagamangala <nagamangala.giridhar@gmail.com>
2025-11-24 09:31:24 +0530

    Updated digit flipping logic, correct '2'-pixelmap
    Updated digit flipping logic in module bin2bcd and reversed the display image for number '2' in VGA ROM module.
    
    Co-Authored-By: paranjaydhadwal <61737796+paranjaydhadwal@users.noreply.github.com>

--- bin2bcd.v
+++ bin2bcd.v
@@ -1,19 +1,23 @@
-module bin2bcd #(
-    parameter W = 16 // input width
-)(
-    input wire [W-1:0] bin, // binary
-    output reg [W+(W-4)/3:0] bcd // bcd {...,thousands,hundreds,tens,ones}
-);
+module bin2bcd #( 
+    parameter W = 16 // input width 
+)( 
+    input wire [W-1:0] bin, // binary 
+    output reg [W+(W-4)/3:0] bcd // bcd {...,thousands,hundreds,tens,ones} 
+); 
 
-  integer i,j;
+  integer i,j; 
+  reg [W+(W-4)/3:0] bcd_temp; // temporary storage
 
-  always @(bin) begin
-    for(i = 0; i <= W+(W-4)/3; i = i+1) bcd[i] = 0;           // initialize with zeros
-    bcd[W-1:0] = bin;                                         // initialize with input vector
-    for(i = 0; i <= W-4; i = i+1)                             // iterate on structure depth
-        for(j = 0; j <= i/3; j = j+1)                         // iterate on structure width
-            if (bcd[W-i+4*j -: 4] > 4)                        // if > 4
-                bcd[W-i+4*j -: 4] = bcd[W-i+4*j -: 4] + 4'd3; // add 3
-  end
+  always @(bin) begin 
+    for(i = 0; i <= W+(W-4)/3; i = i+1) bcd_temp[i] = 0;      
+    bcd_temp[W-1:0] = bin;                                    
+    for(i = 0; i <= W-4; i = i+1)                             
+        for(j = 0; j <= i/3; j = j+1)                         
+            if (bcd_temp[W-i+4*j -: 4] > 4)                   
+                bcd_temp[W-i+4*j -: 4] = bcd_temp[W-i+4*j -: 4] + 4'd3;
+    
+    // Reverse the order: concatenate from LSB to MSB
+    bcd = {1'b0,bcd_temp[3:0], bcd_temp[7:4], bcd_temp[11:8], bcd_temp[15:12], bcd_temp[19:16]};
+  end 
 
 endmodule
